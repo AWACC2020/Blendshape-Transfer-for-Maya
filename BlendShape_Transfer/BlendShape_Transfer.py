a6ffc249396f50b6397204ad29530cd3f4fae0b2
AWACC2020 <59757335+AWACC2020@users.noreply.github.com>
2021-10-09 23:48:11 +0800

    Update BlendShape_Transfer.py

--- BlendShape_Transfer/BlendShape_Transfer.py
+++ BlendShape_Transfer/BlendShape_Transfer.py
@@ -1,7 +1,7 @@
 # -!- coding: utf-8 -!-
 # Author  : AWACS
 # Time	: 2020/10/20
-# version : 0.52 beta
+# version : 0.55 beta
 
 import maya.cmds as cmds
 import os
@@ -37,7 +37,7 @@ def BlendShape_Transfer_GUI():
 
     cmds.rowLayout(nc = 6)
     cmds.text(label = " 1: ")
-    cmds.text(label = "Transfer Method XX:  ", w = 200, align = "right")
+    cmds.text(label = "Transfer Method XX:  ", w = 240, align = "right")
     cmds.optionMenu( "Transfer_Method" , w = 180 )
     cmds.menuItem( label='Wrap deformer' )
     # cmds.menuItem( label='Sorry,No others yet,I may add more later' )
@@ -46,7 +46,7 @@ def BlendShape_Transfer_GUI():
 
     cmds.rowLayout(nc = 6)
     cmds.text(label = " 2: ")
-    cmds.text(label = "Select Original Mesh ( with old BS Node ) :  ", w = 200, align = "right")
+    cmds.text(label = "Select Original Mesh ( with old BS Node ) :  ", w = 240, align = "right")
     cmds.textField('Input_Source_Mesh', w = 140, h = 24, text = "")
     cmds.button(c = lambda *args: Set_Source_Geo(), label = "Confirm", w = 50)
     # Question_Button("Question_Window( 0 )", 10)
@@ -54,21 +54,21 @@ def BlendShape_Transfer_GUI():
 
     cmds.rowLayout(nc = 6)
     cmds.text(label = " 3: ")
-    cmds.text(label = "Select Target Mesh  ( Transfer to Where ) :  ", w = 200, align = "right")
+    cmds.text(label = "Select Target Mesh  ( Transfer to Where ) :  ", w = 240, align = "right")
     cmds.textField('Input_Target_Mesh', w = 140, h = 24, text = "")
     cmds.button(c = lambda *args: Set_Target_Geo(), label = "Confirm", w = 50)
     cmds.setParent('..')
 
     cmds.rowLayout(nc = 6)
     cmds.text(label = " 4: ")
-    cmds.text(label = "Set Source Blendshape (Optional) :  ", w = 200, align = "right")
+    cmds.text(label = "Set Source Blendshape (Optional) :  ", w = 240, align = "right")
     cmds.textField('InputBS', w = 140, h = 24, text = "")
     cmds.button(c = lambda *args: Set_Source_BS(), label = "Confirm", w = 50)
     cmds.setParent('..')
 
     cmds.rowLayout(nc = 6)
     cmds.text(label = " 5: ")
-    cmds.text(label = " New Blendshape Name :  ", w = 200, align = "right")
+    cmds.text(label = " New Blendshape Name :  ", w = 240, align = "right")
     cmds.textField('New_BS_Name', w = 180, h = 24, text = "BS_Transfered")
     cmds.setParent('..')
 
@@ -76,7 +76,7 @@ def BlendShape_Transfer_GUI():
     cmds.text(label = " 6: ")
     # cmds.text(label = "Add to Existing Blendshape Node : ", w = 140)
     
-    cmds.checkBox( "Add_to_exist_BS" , l ="     Add to existing Blendshape :  ", value = False, w = 200, align = "right")
+    cmds.checkBox( "Add_to_exist_BS" , l ="     Add to existing Blendshape :  ", value = False, w = 240, align = "right")
     # cmds.text(label = " Existing_BS :  " ,w = 80)
     cmds.textField('Existing_BS', w = 140, h = 24, text = "")
     cmds.button(c = lambda *args: Set_Existing_BS(), label = "Confirm", w = 50)
@@ -91,7 +91,7 @@ def BlendShape_Transfer_GUI():
     cmds.text(label = " 7: ")
     # cmds.text(label = "Add to Existing Blendshape Node : ", w = 140)
     
-    cmds.checkBox( "Append_Driven" , l ="     Append_Driven  ", value = False, w = 200, align = "right")
+    cmds.checkBox( "Append_Driven" , l ="     Append_Driven  ", value = False, w = 240, align = "right")
     # cmds.text(label = " Existing_BS :  " ,w = 80)
     cmds.setParent('..')
 
@@ -100,7 +100,6 @@ def BlendShape_Transfer_GUI():
     cmds.text(label = " 8: ")
     cmds.button(c = lambda *args: ExecuteTransferBS(), label = "Execute BlendShape Transfer ", w = 180)
     cmds.setParent('..')
-
 
 def Object_Type_Checker( InputObject, Object_Type ):
     """
@@ -200,30 +199,28 @@ def ExecuteTransferBS():
     Append_Driven = cmds.checkBox( "Append_Driven" , query = 1, value = 1)
     Add_to_Exist_BS = cmds.checkBox( "Add_to_exist_BS" , query = 1, value = 1)
     Existing_BS = cmds.textField( "Existing_BS" , query = 1, text = 1) 
-    if len(Existing_BS) < 1 :
-        if Add_to_Exist_BS:
+
+    if Add_to_Exist_BS:
+        if len(Existing_BS) < 1 :
             Existing_BS = SearchingForBlendshape(Target_Geo)
 
     if Existing_BS is None :
         Add_to_Exist_BS = False
-
 
     if len(Source_BS_Text) > 0:
         Source_BS = Source_BS_Text.split(" ")
     else:
         Source_BS = SearchingForBlendshape(Source_Geo)
 
-    print ("-------------------")
     print(Source_BS)
 
     if Source_BS:
-        print ("SSSSSSSSSSSSSSSSSSSSSSSS")
         for IndSrcBS in Source_BS:
             if IndSrcBS == "" :
                 continue
             if len(IndSrcBS) > 0 :
                 print((Source_Geo, IndSrcBS, Target_Geo, Add_to_Exist_BS , Existing_BS , False, Append_Driven ,NewBSName))
-                BlendShape_Transfer_to_Subdivided_geo(Source_Geo, IndSrcBS, Target_Geo, Add_to_Exist_BS , Existing_BS , False, Append_Driven ,NewBSName)
+                BlendShape_Transfer_Main(Source_Geo, IndSrcBS, Target_Geo, Add_to_Exist_BS , Existing_BS , False, Append_Driven ,NewBSName)
             else:
                 print( "No Blendshape Node Found , please select Blendshape Node And Confirm Manually" )
 
@@ -248,7 +245,6 @@ def Create_Wrap( DriveR_Mesh, DriveN_Mesh, name, threshold = 0, maxDistance = 1.
     cmds.select(cl = 1)
     return wrapNode
 
-
 def Create_BS( SourceGeo, BS_Name ):
     cmds.select(cl = 1)
     cmds.select(SourceGeo)
@@ -263,11 +259,11 @@ def Initialize_BS( BSNode ):
     """
     BS_Node_target_list = cmds.listAttr(BSNode + ".w", k = True, m = True)
     cmds.setAttr(BSNode + ".envelope", 1)
-    try:
-        for indBS_Node_target in BS_Node_target_list:
+    for indBS_Node_target in BS_Node_target_list:
+        try:
             cmds.setAttr(BSNode + '.' + indBS_Node_target, 0)
-    except:
-        print("Initialize BS Func Failed")
+        except:
+            print("Initialize BS Func Failed , Failed to set 0 at ------" + BSNode + '.' + indBS_Node_target)
 
 
 def polySmooth_Custom( InputObject, Level = 1 ):
@@ -335,170 +331,145 @@ def Point_distance_Checker ( inputgeoA , inputgeoB , threshold , faster = True )
         cmds.cycleCheck(e=True)
         return False
 
-def BlendShape_Transfer_to_Subdivided_geo( Source_Geo, BS_Node, Target_Geo, Add_to_Existing_BS, Existing_BS,
-                                           Subdivided_Transfer, Append_Driven , NewBSName = "" ):
+def BlendShape_Transfer_Main( Source_Geo, BS_Node, Target_Geo, Add_to_Existing_BS, Existing_BS,
+                                           Subdivided_Transfer, Append_Driven , NewBSName = "BS_Transfered" ):
     # LowPolySrc , BS_of_LowPolySrc ):, HighPolyGeo , HighPoly_is_Extended , Transfer_Vertex , Transfer_Method , New_name_list ):
     # transfer the low Poly Mesh Blendshape to a Extended High Poly Mesh
-    print ( "Source BS : " + BS_Node )
-
+    #=====================initialize===================
     BS_Node_target_list = cmds.listAttr(BS_Node + ".w", k = True, m = True)
     print (BS_Node_target_list)
+    #=====================initialize===================
+    # if not Append_Driven:
+    #     Initialize_BS(BS_Node)
 
-    if not Append_Driven:
-        Initialize_BS(BS_Node)
+    # ----- recording input Connection of Source BS
+    Target_input_Connections = {}
+    for indTarget in BS_Node_target_list:
+        indTarget_plugs = BS_Node + '.' + indTarget
+        try:
+            indTarget_Driven_input = cmds.listConnections( indTarget_plugs , plugs = True )[0]
+            Target_input_Connections[indTarget] = indTarget_Driven_input
+        except:
+            Target_input_Connections[indTarget] = False
 
-    # Duplicate
-    # if Subdivided_Transfer:
-    # 	polySmooth_Custom(Source_Geo , 1 )
+    # ----- disconnectAttr Source BS
+    for indTarget , indInput_connection in Target_input_Connections.items():
+        indTarget_plugs = BS_Node + '.' + indTarget
+        if indInput_connection :
+            try:
+                cmds.disconnectAttr( indInput_connection , indTarget_plugs)
+            except:
+                print( "Failed to disconnectAttr {} ---- {} ".format( indTarget_plugs , indInput_connection))
 
-    # BS_Target_low = []
-    # for indBS_Node_target in BS_Node_target_list:
-    # 	cmds.setAttr(BS_Node + '.' + indBS_Node_target, 1)
-    # 	# tmp_BS_Target = cmds.duplicate( Source_Geo , n = Source_Geo + "_Temp_" + indBS_Node_target )
-    # 	cmds.setAttr(BS_Node + '.' + indBS_Node_target, 0)
-    # 	# BS_Target_low.append(tmp_BS_Target[0])
-    # 	# print (type(tmp_BS_Target))
+    Initialize_BS(BS_Node)
 
-    # tmp = 50
-    # for ind_BS_Target_Low in BS_Target_low:
-    # 	cmds.setAttr(ind_BS_Target_Low + '.translateX', tmp)
-    # 	tmp += 50
+    #=====================initialize End===================
 
-    # Warp Readying
+    #=====================Wraping Start===================
+    #1first Duplicate a Target Geo
     Target_Geo_Transfer_Ready = cmds.duplicate(Target_Geo, n = Target_Geo + "_Temp_Transfer_Ready")[0]
+
+    # ----------- duplicate entire Source geometry with upstreamNodes is safer ?
     # Duplicated_Wrap_Src = cmds.duplicate(Source_Geo, upstreamNodes = True , renameChildren = True )
-    Duplicated_Wrap_Src = [ Source_Geo ]
-
-    # return 0
-    # Duplicated_Wrap_Src = []
-
-    Wrap_Src_Geo = Duplicated_Wrap_Src[0]
-
-    # ------------------------------------------------------------Search in list of duplicated object
-    # aborted
-    # Wrap_Src_BS = ""
-    # possibleBs = [ ]
-    # for indObject in Duplicated_Wrap_Src:
-    #     if Object_Type_Checker(indObject, "blendShape"):
-    #         # confirming Duplicated Bs Node ##might be some issue
-    #         # Wrap_Src_BS = indObject
-    #         possibleBs.append ( indObject )
-    #         # break
-    # for indpossibleBs in possibleBs :
-    #     if indpossibleBs.split(":")[-1] == BS_Node.split(":") :
-    #         print ("Duplicated BS SRC Located : " + indpossibleBs )
-    #         print (indpossibleBs)
-    #         Wrap_Src_BS = indpossibleBs
+    # Duplicated_Wrap_Src = [ Source_Geo ]
 
 
-    # print (BS_Node)
-    # print (possibleBs)
-    # ------------------------------------------------------------Search in list of duplicated object
-
-    # Wrap_Src_BS = "BS_FAC_TEMP1"
+    Wrap_Src_Geo = Source_Geo
     Wrap_Src_BS = BS_Node
 
-    # print("==========================================================================")
-    # print (Duplicated_Wrap_Src)
-    #
-    # if len(Wrap_Src_BS) <1 :
-    #     for indObject in Duplicated_Wrap_Src:
-    #         Wrap_Src_BS = SearchingForBlendshape( indObject )
-    #         if Wrap_Src_BS :
-    #             break
-    
-    print ( "==========================================================================" )
-    print ( Wrap_Src_BS )
-    # return 0
-    # print (Target_Geo_Transfer_Ready)
+    WarpNode = Create_Wrap(Wrap_Src_Geo, Target_Geo_Transfer_Ready, "WarpNode_Temp_For_BS_Transfer", 0, 1.0, False, False, 0)
 
-    WarpNode = Create_Wrap(Wrap_Src_Geo, Target_Geo_Transfer_Ready, "WarpNode", 0, 1.0, False, False, 0)
+    # --------- get individual wrap result
     Duped_Target_Geo_list = []
-    
-    driven_source_list = []
-
     for indTarget in BS_Node_target_list:
-        if Append_Driven:
-            indTarget_Driven_input = cmds.listConnections(  Wrap_Src_BS + '.' + indTarget  , plugs = True )[0]
-            print ( indTarget_Driven_input )
-            print ( Wrap_Src_BS + '.' + indTarget )
-            cmds.disconnectAttr( indTarget_Driven_input , Wrap_Src_BS + '.' + indTarget )
 
         cmds.setAttr(Wrap_Src_BS + '.' + indTarget, 1)
         # cmds.setAttr(WarpNode + ".envelope", 0)
         # cmds.setAttr(WarpNode + ".envelope", 1)
-        tmp_BS_Target = cmds.duplicate(Target_Geo_Transfer_Ready, n = Target_Geo_Transfer_Ready + "_Temp_" + indTarget)
+        ind_tmp_BS_Target = cmds.duplicate( Target_Geo_Transfer_Ready, n = Target_Geo_Transfer_Ready + "_Temp_" + indTarget)
 
         cmds.setAttr(Wrap_Src_BS + '.' + indTarget, 0)
-        Duped_Target_Geo_list.append(tmp_BS_Target[0])
+        Duped_Target_Geo_list.append(ind_tmp_BS_Target[0])
 
-        if Append_Driven:
-            cmds.connectAttr( indTarget_Driven_input , Wrap_Src_BS + '.' + indTarget , f=1)
-            driven_source_list.append(indTarget_Driven_input)
+    # ----- reconnectAttr Source BS
+    for indTarget , indInput_connection in Target_input_Connections.items():
+        indTarget_plugs = Wrap_Src_BS + '.' + indTarget
+        if indInput_connection :
+            try:
+                cmds.connectAttr( indInput_connection , indTarget_plugs , f=1)
+            except:
+                print( "Failed to disconnectAttr {} ---- {} ".format( indTarget_plugs , indInput_connection))
+                pass
 
+    cmds.delete(WarpNode)
+    cmds.delete(Target_Geo_Transfer_Ready)
+    #=====================Wraping End===================
+    #=====================Target BS setting up Start===================
+    Target_Index = 0
 
     if Add_to_Existing_BS:
         FinalBS = Existing_BS[0]
         Existing_BS_Node_target_list = cmds.listAttr( FinalBS + ".w", k = True, m = True)
         print( Existing_BS_Node_target_list )
+        # ----- increaseing after 2
+        Target_Index += len(Existing_BS_Node_target_list) + 2
     else:
         if len(NewBSName) < 1:
             FinalBS = Create_BS(Target_Geo, Target_Geo + "_BS")
         else:
             FinalBS = Create_BS(Target_Geo, NewBSName)
 
-    # if Subdivided_Transfer:
-    #	 for indTarget in Duped_Target_Geo_list:
-    #		 cmds.polySmooth( indTarget , constructionHistory = False, ost = 0, khe = 0, ps = 0.1, kmb = 1, bnr = 1, mth = 0,
-    #						 suv = 1, peh = 0, ksb = 1, ro = 1, sdt = 2, ofc = 0, kt = 1, ovb = 1, dv = 1, ofb = 3, kb = 1,
-    #						 c = 1, ocr = 0, dpe = 1, sl = 1)
-
-
-    # Stright bs
-
-    Targer_Index = 0
-    if Add_to_Existing_BS:
-        Targer_Index += len(Existing_BS_Node_target_list) + 2# increaseing after 2
-    print (FinalBS)
-
+    print ("Transfer to Blendshape : Blendshape name : {}".format (FinalBS) )
+    #=====================Target BS setting up End===================
+    
+    #=====================Add Target to New BS Start===================
     cmds.select(cl = 1)
     for indTargetindex in range(len(Duped_Target_Geo_list)):
-        BypassUnuseful_target = True
-        print("transfering : {Transfering} ".format(Transfering = BS_Node_target_list[indTargetindex] ))
+        print("transfering : {} ".format( BS_Node_target_list[indTargetindex] ))
+        ind_tmp_BS_Target = Duped_Target_Geo_list[indTargetindex]
+        BypassUnuseful_target = False
+        
         if BypassUnuseful_target:
-
-            mismatch = Point_distance_Checker( Target_Geo , Duped_Target_Geo_list[indTargetindex] , 0.02 )
+            mismatch = Point_distance_Checker( Target_Geo , ind_tmp_BS_Target , 0.02 )
             if mismatch is False :
                 print("below point distance check threshold , by passed ")
                 continue
 
-            Target_Geo_Shape = cmds.listRelatives( Target_Geo , shapes = True )[0]
+        Target_Geo_Shape = cmds.listRelatives( Target_Geo , shapes = True )[0]
 
-            cmds.select(Duped_Target_Geo_list[indTargetindex])
-            cmds.blendShape( FinalBS , e = 1, tc = True, w = (Targer_Index, 0),
-                            t = ( Target_Geo , Targer_Index, Duped_Target_Geo_list[indTargetindex] + 'Shape', 1))
+        cmds.select( ind_tmp_BS_Target )
+        cmds.blendShape( FinalBS , e = 1, tc = True, w = (Target_Index, 0),
+                        t = ( Target_Geo , Target_Index, ind_tmp_BS_Target + 'Shape', 1))
 
-            cmds.aliasAttr(BS_Node_target_list[ indTargetindex ], FinalBS + '.w[' + str(Targer_Index) + ']')
-        
+        indTarget = BS_Node_target_list[ indTargetindex ]
+        indTarget_name = indTarget
+        try:
+            cmds.aliasAttr(indTarget_name, FinalBS + '.w[' + str(Target_Index) + ']')
+        except:
+            print( "name conflict might appear : " + indTarget_name )
+            indTarget_name = indTarget_name + '_Name_Conflict'
+            cmds.aliasAttr(indTarget_name , FinalBS + '.w[' + str(Target_Index) + ']')
+            
+        #=====================Addend driven attr Start===================
         if Append_Driven:
-            try:
-                # cmds.connectAttr('LeftFoot_poseInterpolatorShape.output[3]', 'KHU_SOC_BS.LeftArm_0_0_30HULL_SUBED_0_0_0', f=1)
+            indTarget_plugs = FinalBS + '.' + indTarget
+            if Target_input_Connections[ indTarget ] :
+                try:
+                    cmds.connectAttr( Target_input_Connections[ indTarget ] , indTarget_plugs , f=1)
+                except:
+                    print( "Failed to disconnectAttr {} ---- {} ".format( Target_input_Connections[ indTarget ] , indTarget_plugs))
+                    pass
+        #=====================Addend driven attr End===================
 
-                cmds.connectAttr( driven_source_list[indTargetindex] , FinalBS + "." + BS_Node_target_list[ indTargetindex ], f=1)
-            except:
-                print ( " connectAttr Failed : " + driven_source_list[indTargetindex] +" ------------- "+  FinalBS + "." + BS_Node_target_list[ indTargetindex ] )
-
-        Targer_Index += 1
+        Target_Index += 1
         cmds.select(cl = 1)
+    #=====================Add Target to New BS End ===================
 
-    # clean up
-    print ("===================================================")
-    return
-    print (WarpNode)
-    cmds.delete(WarpNode)
-    cmds.delete(Target_Geo_Transfer_Ready)
-    # cmds.delete(Duplicated_Wrap_Src)
+    #===================== clean up ===================
+    print ("cleanup ===================================================")
+    
     cmds.delete(Duped_Target_Geo_list)
+
 
 # From PSD_Point_Snap_GUI_2
 # removed AxisLimit input
